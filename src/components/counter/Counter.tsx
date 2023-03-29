e6ca91b054e76ba3cf2115484f244ce30bd448b3
waleedbinalim <waleedbinalim@gmail.com>
2023-03-30 01:22:35 +0500

    chore: prettier setup

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,11 +1,10 @@
 import { createSignal } from "solid-js";
-// import "./Counter.css";
 
 export const Counter = () => {
   const [count, setCount] = createSignal(0);
   return (
     <button
-      class="increment text-white  bg-black shadow-lg"
+      class="increment bg-black  text-white shadow-lg"
       onClick={() => setCount(count() + 1)}
     >
       Clicks: {count()}
