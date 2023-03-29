af437b93256eead8533c690022cc042d0eafa34b
waleedbinalim <waleedbinalim@gmail.com>
2023-03-30 02:18:21 +0500

    chore: eslint base setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {},
+};
