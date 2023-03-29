138b6b445ac0335b1a3fc6573e1117723ca4cf51
waleedbinalim <waleedbinalim@gmail.com>
2023-03-30 03:04:54 +0500

    chore: added motion one for solid

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,13 +1,18 @@
 import { createSignal } from "solid-js";
+import { Motion } from "@motionone/solid";
 
 export const Counter = () => {
   const [count, setCount] = createSignal(0);
   return (
-    <button
-      class="increment bg-black text-white  shadow-lg"
-      onClick={() => setCount(count() + 1)}
-    >
-      Clicks: {count()}
-    </button>
+    <>
+      <Motion.button
+        animate={{ opacity: [0, 1], x: [-100, 0] }}
+        transition={{ duration: 0.5, easing: "ease-in-out" }}
+        class="rounded-md bg-blue-100 px-3 py-1 text-gray-400  shadow-lg"
+        onClick={() => setCount(count() + 1)}
+      >
+        Clicks: {count()}
+      </Motion.button>
+    </>
   );
 };
