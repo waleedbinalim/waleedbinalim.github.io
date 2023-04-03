d27f454d9a0be7a532d136654f0933f690a3dbc5
waleedbinalim <waleedbinalim@gmail.com>
2023-04-04 04:32:46 +0500

    xl viewports covered for the hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -12,14 +12,15 @@ export const Hero = () => {
   return (
     <>
       <section class="w-100 bg-black">
-        <div class="w-100 align-center flex min-w-full flex-wrap items-center justify-center px-36 py-8 pt-20 text-white sm:justify-between">
+        <div class="w-100 align-center flex min-w-full flex-wrap items-center justify-center px-36 py-8 pt-20 text-white sm:justify-between xl:px-60">
           <div class="w-100 text-center md:text-left">
-            <p class="text-2xl font-semibold">
-              Hi, I'm <span class="text-4xl text-green-400">WALEED</span>
+            <p class="text-2xl font-semibold xl:pb-2 xl:text-5xl">
+              Hi, I'm{" "}
+              <span class="text-4xl text-green-400 lg:text-6xl">WALEED</span>
             </p>
-            <p class="mb-4 min-w-[300px] text-xl">
+            <p class="mb-4 min-w-[300px] text-xl lg:text-3xl">
               I do{" "}
-              <span class=" text-2xl font-bold text-green-400">
+              <span class=" text-2xl font-bold text-green-400 lg:text-3xl">
                 <SolidTyper
                   text={[
                     "Web Development",
@@ -35,14 +36,14 @@ export const Hero = () => {
               </span>
             </p>
 
-            <div class="md:pb-none pb-8 md:min-w-[140px] md:max-w-[170px]">
-              <button class="w-[200px] rounded-full border-2 border-green-400 px-4 py-2 font-bold text-green-400 transition-colors hover:bg-green-400 hover:text-gray-800 md:w-full">
+            <div class="md:pb-none pb-8 md:min-w-[140px] md:max-w-[170px] xl:max-w-[250px]">
+              <button class="w-[200px] rounded-full border-2 border-green-400 px-4 py-2 font-bold text-green-400 transition-colors hover:bg-green-400 hover:text-gray-800 md:w-full xl:text-2xl">
                 View Resume
               </button>
               <div class="flex w-full justify-center pt-3">
                 <For each={heroSocials}>
                   {(item) => (
-                    <div class="cursor-pointer fill-green-400 px-1 opacity-50 hover:opacity-100 [&_path]:!fill-gray-200 [&_svg]:!h-6 [&_svg]:!w-6">
+                    <div class="cursor-pointer fill-green-400 px-1 opacity-50 hover:opacity-100 [&_path]:!fill-gray-200 [&_svg]:h-6 [&_svg]:w-6 xl:[&_svg]:h-8 xl:[&_svg]:w-8 ">
                       <a href={item.href} target="_blank">
                         {item.icon}
                       </a>
@@ -53,11 +54,11 @@ export const Hero = () => {
             </div>
           </div>
 
-          <div class="w-100 relative flex h-[200px] min-w-[200px] sm:h-[320px] sm:min-w-[300px]">
-            <div class="absolute left-0 top-0 h-full w-[200px] rotate-12 rounded-xl bg-green-300 sm:h-80 sm:w-[300px]" />
-            <div class="absolute left-0 top-0 h-full w-[200px] rotate-6 rounded-xl bg-green-400 drop-shadow-xl sm:h-80 sm:w-[300px]" />
+          <div class="w-100 relative flex h-[200px] min-w-[200px] sm:h-[320px] sm:min-w-[300px] xl:h-[400px] xl:min-w-[400px]">
+            <div class="absolute left-0 top-0 h-full w-[200px] rotate-12 rounded-xl bg-green-300 sm:h-80 sm:w-[300px] xl:h-full xl:w-[400px]" />
+            <div class="absolute left-0 top-0 h-full w-[200px] rotate-6 rounded-xl bg-green-400 drop-shadow-xl sm:h-80 sm:w-[300px] xl:h-full xl:w-[400px]" />
             <img
-              class="absolute left-0 top-0 rounded-xl border-2 border-green-400 drop-shadow-xl sm:h-80"
+              class="absolute left-0 top-0 rounded-xl border-2 border-green-400 drop-shadow-xl sm:h-80 xl:h-full"
               src={imgUrls.heroAvatar}
               alt="waleed-avatar"
             />
