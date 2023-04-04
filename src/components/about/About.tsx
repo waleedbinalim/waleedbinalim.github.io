c88a5c6fdc4feb84704dcf6ee5e2c695b743e836
waleedbinalim <waleedbinalim@gmail.com>
2023-04-05 04:47:18 +0500

    chore: work section fixed with better classes

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -26,40 +26,29 @@ export const About: Component = () => {
         <WaveOne />
       </div>
 
-      <h1 class="bg-gray-900 px-24 pb-14 text-center text-3xl font-bold text-slate-200 sm:px-36 sm:text-4xl">
+      <h1 class="bg-gray-900 px-24 pb-14 text-center text-3xl font-bold text-slate-200 sm:px-36 sm:pb-24 sm:text-4xl">
         WORK
       </h1>
 
       <section class="bg-gray-900 px-14 pb-12 text-slate-200 sm:px-36">
-        <For each={careerInfo}>
-          {(company, i) => (
-            <div
-              class="flex w-full items-baseline justify-start px-4 py-4 pb-8 text-center sm:text-left"
-              classList={{
-                ["sm:justify-end sm:[&>div]:w-auto"]: (+i() + 1) % 2 === 0,
-              }}
-            >
+        <div class="grid text-left sm:grid-cols-2 sm:gap-24 sm:text-left">
+          <For each={careerInfo}>
+            {(company, i) => (
               <div
-                class="w-full "
-                classList={{ ["sm:[&>div]:w-auto"]: (+i() + 1) % 2 === 0 }}
+                class="mt pb-16 sm:h-full "
+                classList={{ ["sm:mt-32"]: (+i() + 1) % 2 === 0 }}
               >
-                <div class="ms:text-3xl flex items-center justify-center text-center text-2xl font-bold text-slate-200 sm:justify-start sm:pl-4 sm:text-left sm:text-4xl">
-                  <div class="hidden h-8 w-8 items-center justify-center rounded-full border-4 border-slate-300 sm:flex">
-                    <div class=" h-4 w-4 rounded-full bg-slate-300" />
-                  </div>
-                  <p class="w-full sm:pl-4">{company.company}</p>
-                </div>
-
-                <p class="text-md font-bold text-green-400 sm:pl-16 sm:text-xl">
+                <p class="flex text-3xl font-bold sm:text-4xl">
+                  {company.company}
+                </p>
+                <p class="text-2xl font-semibold text-green-400">
                   {company.position}
                 </p>
-                <p class="text-sm text-slate-100 sm:pl-16 sm:text-lg">
-                  {company.time}
-                </p>
+                <p class="text-md text-slate-100">{company.time}</p>
               </div>
-            </div>
-          )}
-        </For>
+            )}
+          </For>
+        </div>
       </section>
       <div>
         <div class="w-100 bg-black pb-8 pt-12 text-center text-4xl font-bold text-slate-200">
