3b016773d3d9405ffd6e5b32059f35d7371dfb95
waleedbinalim <waleedbinalim@gmail.com>
2023-04-13 00:51:00 +0500

    improved projects section approach

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
   return (
     <>
       <main>
-        <Title>Hello World</Title>
+        <Title>Waleed Bin Alim</Title>
         <Hero />
         <About />
         <Projects />
