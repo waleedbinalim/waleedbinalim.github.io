d9ac542ec0067ac7da15f2cedce81d0504ed0521
waleedbinalim <waleedbinalim@gmail.com>
2023-04-24 17:31:15 +0500

    moaaar icons and that pretty animation, i have svg manipulation tho

--- src/assets/skill-logos/index.ts
+++ src/assets/skill-logos/index.ts
@@ -1,4 +1,21 @@
 import BlenderIcon from "./blender.svg";
 import ReactIcon from "./react.svg";
+import FramerMotionIcon from "./framer-motion.svg";
+import NextJSIcon from "./nextjs-logo.svg";
+import TailwindIcon from "./tailwind.svg";
+import NodeJSIcon from "./node.svg";
+import MongoDBIcon from "./mongo.svg";
+import VueIcon from "./vue.svg";
+import SolidJSIcon from "./solid.svg";
 
-export { BlenderIcon, ReactIcon };
+export {
+  BlenderIcon,
+  ReactIcon,
+  FramerMotionIcon,
+  NextJSIcon,
+  TailwindIcon,
+  NodeJSIcon,
+  MongoDBIcon,
+  VueIcon,
+  SolidJSIcon,
+};
--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,6 +1,17 @@
 import { Motion } from "@motionone/solid";
 import { Component, For } from "solid-js";
-import { BlenderIcon, ReactIcon, WaveOne } from "~/assets";
+import {
+  BlenderIcon,
+  FramerMotionIcon,
+  MongoDBIcon,
+  NextJSIcon,
+  NodeJSIcon,
+  ReactIcon,
+  SolidJSIcon,
+  TailwindIcon,
+  VueIcon,
+  WaveOne,
+} from "~/assets";
 
 export const About: Component = () => {
   const careerInfo = [
@@ -21,6 +32,18 @@ export const About: Component = () => {
     },
   ];
 
+  const logos = [
+    <BlenderIcon />,
+    <ReactIcon />,
+    <FramerMotionIcon />,
+    <NextJSIcon />,
+    <TailwindIcon />,
+    <NodeJSIcon />,
+    <MongoDBIcon />,
+    <VueIcon />,
+    <SolidJSIcon />,
+  ];
+
   return (
     <>
       <div class="w-100 bg-black [&_path]:!fill-gray-900">
@@ -28,7 +51,7 @@ export const About: Component = () => {
       </div>
 
       <section class="bg-gray-900 px-14 pb-12 text-slate-200 sm:px-36">
-        <div class="grid grid-cols-2 text-left sm:gap-16 sm:text-left">
+        <div class="grid grid-cols-1 text-left sm:grid-cols-2 sm:gap-16 sm:text-left">
           <div>
             <h1 class="pb-12 text-left text-3xl font-bold text-slate-200 sm:text-4xl">
               WORK
@@ -53,57 +76,37 @@ export const About: Component = () => {
               Skills
             </h1>
             <div>
-              <Motion
-                style={{ display: "inline-block" }}
-                animate={{
-                  transform: [
-                    "rotate(-22.5deg)",
-                    "rotate(0deg)",
-                    "rotate(22.50deg)",
-                    "rotate(0deg)",
-                    "rotate(-22.5deg)",
-                  ],
-                }}
-                transition={{
-                  duration: 3,
-                  easing: "linear",
-                  repeat: Infinity,
-                }}
-              >
-                <BlenderIcon />
-              </Motion>
-              <Motion
-                style={{ display: "inline-block" }}
-                animate={{
-                  transform: [
-                    "rotate(-22.5deg)",
-                    "rotate(0deg)",
-                    "rotate(22.50deg)",
-                    "rotate(0deg)",
-                    "rotate(-22.5deg)",
-                  ],
-                }}
-                transition={{
-                  duration: 3,
-                  delay: 0.5,
-                  easing: "linear",
-                  repeat: Infinity,
-                }}
-              >
-                <ReactIcon />
-              </Motion>
+              <For each={logos}>
+                {(logo, i) => (
+                  <>
+                    <Motion
+                      class="px-1"
+                      style={{ display: "inline-block" }}
+                      animate={{
+                        transform: [
+                          "rotate(0deg)",
+                          "rotate(0deg)",
+                          "rotate(0deg)",
+                          "rotate(22.50deg)",
+                          "rotate(0deg)",
+                        ],
+                      }}
+                      transition={{
+                        delay: i() * 0.25,
+                        duration: 2,
+                        easing: "linear",
+                        repeat: Infinity,
+                      }}
+                    >
+                      {logo}
+                    </Motion>
+                  </>
+                )}
+              </For>
+
               <ul>
-                <li>Blender</li>
                 <li>Arduino</li>
-                <li>React</li>
-                <li>Framer Motion</li>
                 <li>MySQL</li>
-                <li>NextJS</li>
-                <li>Tailwind</li>
-                <li>Node</li>
-                <li>Mongo</li>
-                <li>Solid</li>
-                <li>Vue</li>
                 <li>Inkscape</li>
                 <li>TS</li>
               </ul>
