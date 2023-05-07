9078c2422455e3a95746f69e8249240a3e6896e2
waleedbinalim <waleedbinalim@gmail.com>
2023-05-07 15:41:36 +0500

    contact fix, hero fix

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,11 +2,11 @@ import { Component } from "solid-js";
 
 export const Contact: Component = () => {
   return (
-    <div class="py-18 sm:px-18 bg-black px-4 pt-24 md:px-36">
+    <div class="py-18 sm:px-18 bg-black  pt-24 md:px-36">
       <div class="mb-8 text-center text-4xl font-bold text-white sm:text-left">
         Contact
       </div>
-      <form class="w-80 pb-24 sm:px-0">
+      <form class="w-70 px-8 pb-24 sm:w-80 sm:px-0">
         <div class="mb-6">
           <label for="email" class="mb-2 text-sm font-medium text-white">
             Email
@@ -14,7 +14,7 @@ export const Contact: Component = () => {
           <input
             type="email"
             id="email"
-            class="w-full rounded-lg border border-gray-600 bg-gray-100 p-2.5 text-sm  placeholder-gray-400 outline-none"
+            class="block w-full rounded-lg border border-gray-600 bg-gray-100 p-2.5 text-sm  placeholder-gray-400 outline-none"
             placeholder="email@domain.com"
             required
           />
@@ -25,7 +25,7 @@ export const Contact: Component = () => {
           </label>
           <textarea
             id="password"
-            class="w-full rounded-lg border border-gray-600 bg-gray-100 p-2.5 text-sm placeholder-gray-400 outline-none"
+            class="block w-full rounded-lg border border-gray-600 bg-gray-100 p-2.5 text-sm placeholder-gray-400 outline-none"
             required
           />
         </div>
