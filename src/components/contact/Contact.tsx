f1956a03166e87fbf743fe75fdb1bc5ddd1d42e1
waleedbinalim <waleedbinalim@gmail.com>
2023-05-07 15:12:09 +0500

    chore: contact responsive

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,36 +3,35 @@ import { Component } from "solid-js";
 export const Contact: Component = () => {
   return (
     <div class="py-18 px-18 bg-black pt-24 md:px-36">
-      <div class="mb-8 text-4xl font-bold text-white">Contact</div>
-      <form class="w-80 pb-24">
+      <div class="mb-8 text-center text-4xl font-bold text-white sm:text-left">
+        Contact
+      </div>
+      <form class="w-80 px-8 pb-24 sm:px-0">
         <div class="mb-6">
-          <label for="email" class="mb-2 block text-sm font-medium text-white">
+          <label for="email" class="mb-2 text-sm font-medium text-white">
             Email
           </label>
           <input
             type="email"
             id="email"
-            class="block w-full rounded-lg border border-gray-600 bg-gray-700 p-2.5  text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
-            placeholder="name@flowbite.com"
+            class="w-full rounded-lg border border-gray-600 bg-gray-100 p-2.5 text-sm  placeholder-gray-400 outline-none"
+            placeholder="email@domain.com"
             required
           />
         </div>
         <div class="mb-6">
-          <label
-            for="password"
-            class="mb-2 block text-sm font-medium text-white"
-          >
+          <label for="password" class="mb-2 text-sm font-medium text-white">
             Message
           </label>
           <textarea
             id="password"
-            class="block w-full rounded-lg border border-gray-600 bg-gray-700 p-2.5  text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
+            class="w-full rounded-lg border border-gray-600 bg-gray-100 p-2.5 text-sm placeholder-gray-400 outline-none"
             required
           />
         </div>
         <button
           type="submit"
-          class="w-[200px] rounded-full border-2 border-green-400 px-4 py-2 font-bold text-green-400 transition-colors hover:bg-green-400 hover:text-gray-800 md:w-full xl:text-2xl"
+          class="w-full rounded-full border-2 border-green-400 px-4 py-2 font-bold text-green-400 transition-colors hover:bg-green-400 hover:text-gray-800 sm:w-[200px] md:w-full xl:text-2xl"
         >
           Submit
         </button>
