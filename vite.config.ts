73b402ba486cbd2c7c90d1888fb6cdd61e6616a3
waleedbinalim <waleedbinalim@gmail.com>
2023-05-20 20:19:08 +0500

    chore: github pages work

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import solid from "solid-start/vite";
 import solidSvg from "vite-plugin-solid-svg";
 import { defineConfig } from "vite";
+import staticAdapter from "solid-start-static";
 
 export default defineConfig({
-  plugins: [solid(), solidSvg()],
+  plugins: [solid({ adapter: staticAdapter() }), solidSvg()],
 });
