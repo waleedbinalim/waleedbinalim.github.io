5b38e7e518d142a9de74b0b3b2a2579078e3ab1b
waleedbinalim <waleedbinalim@gmail.com>
2025-09-07 23:23:59 +0500

    chore: build worked, but page was gone

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from "@solidjs/start/config";
 import solidSvg from "vite-plugin-solid-svg";
 
 export default defineConfig({
+    ssr: true,
+    server: {
+        baseURL: "/",
+        preset: "github-pages",
+        prerender: {
+            crawlLinks: true,
+            autoSubfolderIndex: true,
+        },
+    },
     vite: {
         plugins: [solidSvg()]
     }
